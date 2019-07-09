7ede4c8d42bfd9f0b8761b7d642f1102a1f204a9
xyfke <kexx7130@mylaurier.ca>
2019-07-09 18:15:02 -0400

    Add some more pages, and made navbar more dynamic

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -3,12 +3,19 @@ import '../style.css';
 
 class NavBar extends React.Component {
 
-    renderItems() {
+    itemsHtml(id, title, menu) {
+        if (menu.includes(id)) {
+            return (
+                <li className="nav-item">
+                    <button className="border-0 navitembtn" onClick={() => this.props.changePage(id)}>{title}</button>
+                </li>
+            );
+        }
+    }
 
+    renderItems(current) {
         return this.props.page.map((p) => (
-            <li className="nav-item">
-                <button className="border-0 navitembtn" onClick={() => this.props.changePage(p.id)}>{p.title}</button>
-            </li>
+            this.itemsHtml(p.id, p.title, this.props.page[current-1].menu)
         ));
     }
 
@@ -17,7 +24,7 @@ class NavBar extends React.Component {
             <div>
                 <nav className="navbar navbar-expand-lg justify-content-end mb-0" id="navbar">
                     <ul className="navbar-nav">
-                        {this.renderItems()}
+                        {this.renderItems(this.props.current)}
                     </ul>
                 </nav>
             </div>
