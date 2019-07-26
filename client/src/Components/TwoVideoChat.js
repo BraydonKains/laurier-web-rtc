e396d8df042942f8daa9974cc28ccd96156021ac
StephPitman <32081012+StephPitman@users.noreply.github.com>
2019-07-25 23:47:41 -0400

    Video Camera Working
    -user's video camera view now works when two person chat is opened
       -currently audio is disabled for editing purposes

--- client/src/Components/TwoVideoChat.js
+++ client/src/Components/TwoVideoChat.js
@@ -1,5 +1,30 @@
 import React from 'react';
 class TwoVideoChat extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+        constraints: { audio: false, video: { width: 300, height: 300 } }
+        };
+    }
+
+    componentDidMount(){
+        const constraints = this.state.constraints;
+
+        navigator.mediaDevices.getUserMedia(this.state.constraints).then((stream) => {
+            const video = document.getElementById("userVideo");
+            const vendorURL = window.URL || window.webkitURL;
+            if ("srcObject" in video) {
+                video.srcObject = stream;
+              } else {
+                video.src = window.URL.createObjectURL(stream);
+              }
+            video.play();
+        }).catch((error) => {
+            console.log(error);
+        })
+
+    }
     render(){
         return(
             <div className="TwoVideoChat">
