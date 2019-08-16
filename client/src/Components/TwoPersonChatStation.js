a1cf6990a84fcd39f5cfd7afb4d45c607bf25be2
StephPitman <32081012+StephPitman@users.noreply.github.com>
2019-08-15 21:02:25 -0400

    Video Chat Process
    -trying to convert JS code to React

--- client/src/Components/TwoPersonChatStation.js
+++ client/src/Components/TwoPersonChatStation.js
@@ -16,7 +16,20 @@ class TwoPersonChatStation extends React.Component{
             text='',
             username='',
             chats=[],
-            menu :[1, 5]
+
+            usersOn={},
+            id={},
+            users={},
+            sessionDesc={},
+            currentCaller={},
+            room:{},
+            caller:{},
+            localUserMedia={},
+            remoteSrc={},
+            selfSrc={},
+
+
+            menu =[1, 5]
         };
 
         // this.state ={
@@ -41,6 +54,7 @@ class TwoPersonChatStation extends React.Component{
         // this.state.channel.bind("pusher:member_added", members=>{this.memberAdd(members);});
         // this.state.channel.bind("pusher:member_removed", members=>{this.memberRemove(members);});
     }
+    
 
     componentDidMount(){
         //NEED TO CHECK IF PROFESSOR
@@ -54,8 +68,70 @@ class TwoPersonChatStation extends React.Component{
         channel.bind('message', data =>{
             this.setState({chats: [...this.state.chats,data], test=''})
         });
+        channel.bind("pusher:subscription_succeeded", members=>{this.subSucceeded(members);});
+        channel.bind("pusher:member_added", members=>{this.memberAdd(members);});
+        channel.bind("pusher:member_removed", members=>{this.memberRemove(members);});
+
         this.handleTextChange = this.handleTextChange.bind(this);
+        GetRTCPeerConnection();
+        GetRTCSessionDescription();
+        GetRTCIceCandidate();
+        prepareCaller();
+    
     }
+
+    /*-----------------------------------------------------------------------------------------FIGURE OUT WHAT TO DO WITH THIS*/
+    callUser(user) {
+        getCam()
+          .then(stream => {
+            if (window.URL) {
+                this.setState({selfSrc:window.URL.createObjectURL(stream)})
+            //   document.getElementById("selfview").src = window.URL.createObjectURL(
+            //     stream
+            //   );
+            } else {
+                this.setState({selfSrc:stream});
+            //   document.getElementById("selfview").src = stream;
+            }
+            toggleEndCallButton();
+            this.state.caller.addStream(stream);
+            localUserMedia = stream;
+            this.state.caller.createOffer().then(function(desc) {
+              this.state.caller.setLocalDescription(new RTCSessionDescription(desc));
+              channel.trigger("client-sdp", {
+                sdp: desc,
+                room: user,
+                from: id
+              });
+              room = user;
+            });
+          })
+          .catch(error => {
+            console.log("an error occured", error);
+          });
+      }
+
+    prepareCaller() {
+        //Initializing a peer connection
+        caller = new window.RTCPeerConnection();
+        //Listen for ICE Candidates and send them to remote peers
+        caller.onicecandidate = function(evt) {
+          if (!evt.candidate) return;
+          console.log("onicecandidate called");
+          onIceCandidate(caller, evt);
+        };
+        //onaddstream handler to receive remote feed and show in remoteview video element
+        caller.onaddstream = function(evt) {
+          console.log("onaddstream called");
+          if (window.URL) {
+            document.getElementById("remoteview").src = window.URL.createObjectURL(
+              evt.stream
+            );
+          } else {
+            document.getElementById("remoteview").src = evt.stream;
+          }
+        };
+      }
 
     handleTextChange(e){
         if(e.keyCode === 13){
@@ -71,80 +147,80 @@ class TwoPersonChatStation extends React.Component{
         }
     }
 
-    // subSucceeded(members){
-    //     //alert backend
-    //         //MAYBE
-    //     //update usersOn
-    //     var userCount = this.state.usersOn;
-    //     this.setState({usersOn:userCount+1});
-    //     //get user id
-    //     this.setState({id:this.state.channel.members.me.id});
-    //     //add user to room
-    //     var usersTemp = [];
-    //     members.each(member=>{
-    //         if(member.id != this.state.id){
-    //             usersTemp.concat(member.id)
-    //         }
-    //     })
-    //     usersTemp.concat(this.state.id);
+    subSucceeded(members){
+        //alert backend
+            //MAYBE
+        //update usersOn
+        var userCount = this.state.usersOn;
+        this.setState({usersOn:userCount+1});
+        //get user id
+        this.setState({id:this.state.channel.members.me.id});
+        //add user to room
+        var usersTemp = [];
+        members.each(member=>{
+            if(member.id != this.state.id){
+                usersTemp.concat(member.id)
+            }
+        })
+        usersTemp.concat(this.state.id);
 
-    //     this.setState({usersOn: usersTemp});
-    //     //alert chat hat this user has entered the room
-    // }
+        this.setState({usersOn: usersTemp});
+        //alert chat hat this user has entered the room
+    }
 
-    // memberAdd(member){
-    //     //send message in chat
-    //     //add them to users
-    //     this.setState({usersOn:this.state.users.concat(member.id)});
+    memberAdd(member){
+        //send message in chat
+        //add them to users
+        this.setState({usersOn:this.state.users.concat(member.id)});
         
-    // }
+    }
 
-    // memberRemove(member){
-    //     //send message in chat
-    //     //remove them from users
-    //     var usersTemp = this.state.users;
-    //     usersTemp.splice(usersTemp.indexOf(member.id),1);
-    //     this.setState({usersOn:usersTemp});
-    //     if(usersTemp == this.state.id){
-    //         endCall();
-    //     }
-    // }
+    memberRemove(member){
+        //send message in chat
+        //remove them from users
+        var usersTemp = this.state.users;
+        usersTemp.splice(usersTemp.indexOf(member.id),1);
+        this.setState({usersOn:usersTemp});
+        if(usersTemp == this.state.id){
+            endCall();
+        }
+    }
 
-    // getCam() {
-    //     //Get local audio/video feed and show it in selfview video element
-    //     return navigator.mediaDevices.getUserMedia({
-    //       video: true,
-    //       audio: true
-    //     });
-    // }
+    getCam() {
+        //Get local audio/video feed and show it in selfview video element
+        return navigator.mediaDevices.getUserMedia({
+          video: true,
+          audio: true
+        });
+    }
   
-    // GetRTCIceCandidate() {
-    //     window.RTCIceCandidate =
-    //       window.RTCIceCandidate ||
-    //       window.webkitRTCIceCandidate ||
-    //       window.mozRTCIceCandidate ||
-    //       window.msRTCIceCandidate;
+    GetRTCIceCandidate() {
+        window.RTCIceCandidate =
+          window.RTCIceCandidate ||
+          window.webkitRTCIceCandidate ||
+          window.mozRTCIceCandidate ||
+          window.msRTCIceCandidate;
   
-    //     return window.RTCIceCandidate;
-    // }
+        return window.RTCIceCandidate;
+    }
   
-    // GetRTCPeerConnection() {
-    //     window.RTCPeerConnection =
-    //       window.RTCPeerConnection ||
-    //       window.webkitRTCPeerConnection ||
-    //       window.mozRTCPeerConnection ||
-    //       window.msRTCPeerConnection;
-    //     return window.RTCPeerConnection;
-    // }
+    GetRTCPeerConnection() {
+        window.RTCPeerConnection =
+          window.RTCPeerConnection ||
+          window.webkitRTCPeerConnection ||
+          window.mozRTCPeerConnection ||
+          window.msRTCPeerConnection;
+        return window.RTCPeerConnection;
+    }
   
-    // GetRTCSessionDescription() {
-    //     window.RTCSessionDescription =
-    //       window.RTCSessionDescription ||
-    //       window.webkitRTCSessionDescription ||
-    //       window.mozRTCSessionDescription ||
-    //       window.msRTCSessionDescription;
-    //     return window.RTCSessionDescription;
-    // }
+    GetRTCSessionDescription() {
+        window.RTCSessionDescription =
+          window.RTCSessionDescription ||
+          window.webkitRTCSessionDescription ||
+          window.mozRTCSessionDescription ||
+          window.msRTCSessionDescription;
+        return window.RTCSessionDescription;
+    }
   
 
     // state = {
@@ -164,7 +240,7 @@ class TwoPersonChatStation extends React.Component{
                             <div className="container">
                                 <div className="row">
                                     <div className="col-4">
-                                        <TwoVideoChat/>
+                                        <TwoVideoChat remoteView={this.state.remoteSrc} selfView={this.state.selfSrc}/>
                                         <ButtonPanel/>
                                     </div>
                                     <div className="col-8">
