e7983582470b9c471646e8ff7b0d784057a90cca
StephPitman <32081012+StephPitman@users.noreply.github.com>
2019-08-22 07:49:29 -0400

    Potential One View Chat Station
    -not able to test in development because my docker not currently working
    -will be attempting to test in another side project
       -any changes needed and found while testing will be added

--- client/src/Components/ButtonPanel.js
+++ client/src/Components/ButtonPanel.js
@@ -1,25 +1,21 @@
 import React from 'react';
-class ButtonPanel extends React.Component{
-    render(){
-        return(
-            <div className="ButtonPanel">
-                <div className="container w-100">
-                    <div className="row">
-                        <div className="col pl-2">
-                            <button className="addButton rounded">+</button>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col p-2 d-inline">
-                            <button className="startButton rounded">Start</button>
-                            <button className="float-right stopButton rounded">Stop</button>
-                        </div>
+export default({endChatFunc})=>{
+    return(
+        <div className="ButtonPanel">
+            <div className="container w-100">
+                <div className="row">
+                    <div className="col pl-2">
+                        {/* <button className="addButton rounded">+</button> */}
                     </div>
                 </div>
-                
-                
+                <div className="row">
+                    <div className="col p-2 d-inline">
+                        <button className="startButton rounded">Start</button>
+                        <button className="float-right stopButton rounded" onClick={endChatFunc}>Exit Chat</button>
+                    </div>
+                </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 export default ButtonPanel;
