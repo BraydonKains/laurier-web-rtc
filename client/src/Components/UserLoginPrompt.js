ad5711e3d31e0b0e27087afa1276e09d8f5a053d
StephPitman <32081012+StephPitman@users.noreply.github.com>
2019-08-26 22:10:58 -0400

    Stuff Changed

--- client/src/Components/UserLoginPrompt.js
+++ client/src/Components/UserLoginPrompt.js
@@ -8,13 +8,16 @@ import TwoVideoChat from './TwoVideoChat';
 import ButtonPanel from './ButtonPanel';
 import NavBar from './NavBar';
 
+
+
 export default({name,password,passOnChange,nameOnChange,closePopup}) => {
+  
   return (
       <div className='popup'>
         <div className='popup_inner'>
           <h1>Login</h1>
-          Nickname:<input type="text" onChange={nameOnChange}/>
-          Chat Password: <input type="text" onChange={passOnChange}/>
+          Nickname:<input type="text" onChange={nameOnChange.bind(this)} value={name}/>
+          Chat Password: <input type="text" onChange={passOnChange.bind(this)} value={password}/>
         <button onClick={closePopup}>Done</button>
         </div>
       </div>
