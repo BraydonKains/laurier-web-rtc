df8f8f7e37ff52b0460ca15704b6f9fb8ea9ff93
Braydon Kains <kainsbraydon@gmail.com>
2019-09-08 14:27:58 -0400

    Added new STUN and TURN servers to try

--- client/src/Components/TwoPersonChatStation.js
+++ client/src/Components/TwoPersonChatStation.js
@@ -276,12 +276,17 @@ class TwoPersonChatStation extends React.Component{
             let call = new window.RTCPeerConnection({
 		iceServers: [
 		    {
-			urls: "stun:stunserver.example.org"
+			"urls": "stun:stun.l.google.com:19302"
 		    },
 		    {
-			urls: 'turn:192.158.29.39:3478?transport=udp',
-			credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-			username: '28224511:1379330808'
+			"urls": [ 
+			    'turn:webrtcweb.com:7788',
+			    'turn:webrtcweb.com:4455?transport=udp',
+			    'turn:webrtcweb.com:8877?transport=udp',
+			    'turn:webrtcweb.com:8877?transport=tcp'
+			],
+			"credential": 'muazkh',
+			"username": 'muazkh'
 		    }
 		]
 	    });
